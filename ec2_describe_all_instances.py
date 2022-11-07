a3eef7d0aec688013eb4757d86f01423ff353b94
Alonso Montero <amontero1@snapfinance.com>
2022-11-07 15:12:37 -0600

    Added script to describe all instances and list all instance names

---
+++ ec2_describe_all_instances.py
@@ -0,0 +1,23 @@
+import boto3
+import argparse
+
+def parser() -> dict:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--profile', type=str, required=True)
+    parser.add_argument('--region', type=str, required=True)
+
+    return parser.parse_args()
+
+def main():
+    args = parser()
+    boto3.setup_default_session(profile_name=args.profile, region_name=args.region)
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances()
+    print(response)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f'Unexpected error: {e}')
+    
